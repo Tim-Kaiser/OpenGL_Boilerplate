625d8ea37947af1f35c28d58224eec84c53237a9
tk <60766886+Tim-Kaiser@users.noreply.github.com>
2025-04-04 20:31:08 +0200

    compute shaders, clean up (#6)
    Co-authored-by: Siiiiinth <60766886+Siiiiinth@users.noreply.github.com>

--- OpenGL-Boilerplate/ShaderLoader.h
+++ OpenGL-Boilerplate/ShaderLoader.h
@@ -16,21 +16,31 @@ typedef struct
 
 } Shader;
 
+typedef struct
+{
+	GLuint m_shaderProgramID;
+	GLuint m_computeShaderID;
+} ComputeShader;
+
 class ShaderLoader {
 
 public:
 
-	std::unique_ptr<Shader> CreateShaders();
+	// === SHADER ===
 
-	bool CompileShaders(const std::string& filename, GLuint& shaderID);
-	void AttachShaders(Shader& shader);
-	bool LinkProgram(Shader& shader);
+	std::unique_ptr<Shader> CreateShaders(const std::string& vertexShaderFilename, const std::string& fragmentShaderFilename);
 
 	void DetachShaders(Shader& shader);
 	void DestroyShaders(Shader& shader);
-	void DestroyProgram(Shader& shader);
+	void DestroyProgram(GLuint& shaderProgramID);
 
-	GLint GetUniformID(const std::string& uniformName);
+	// === COMPUTE SHADER ===
+
+	std::unique_ptr<ComputeShader> CreateComputeShader(const std::string& computeShaderFilename);
+
+	void DetachShaders(ComputeShader& shader);
+	void DestroyShaders(ComputeShader& shader);
+
 
 	bool SendUniformData(const std::string& uniformName, GLint& data);
 	bool SendUniformData(const std::string& uniformName, GLfloat& data);
@@ -42,4 +52,15 @@ public:
 
 	bool SendUniformData(const std::string& uniformName, const glm::mat4& data);
 	bool SendUniformData(const std::string& uniformName, const glm::vec3& data);
+
+private:
+
+	bool CompileShaders(const std::string& filename, GLuint& shaderID);
+	void AttachShaders(Shader& shader);
+	bool LinkProgram(GLuint& shaderProgramID);
+
+	void AttachShaders(ComputeShader& shader);
+
+	GLint GetUniformID(const std::string& uniformName);
+
 };
